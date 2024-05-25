4fe222cd547276e3520d304268b4035b32abdb41
Hugo Lipko <hugo.lipko@gmail.com>
2024-05-25 23:26:06 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <header></header>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+        <a>test</a>
       </main>
       <footer></footer>
     </div>
