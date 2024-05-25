bf73400900e7a75ae5a37537f8b877ee174ea84e
Hugo Lipko <hugo.lipko@gmail.com>
2024-05-25 22:49:36 +0300

    Initial commit

---
+++ src/app/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <header></header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
